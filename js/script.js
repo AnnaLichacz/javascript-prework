15e4b4635bb5181561e47863589e9edfde64ee2a
Anna Lichacz <anniahod@wp.pl>
2023-02-02 21:47:14 +0100

    Add button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
 
+buttonRock = document.getElementById("button-rock");
+buttonRock.addEventListener("click", function () {
+    buttonClicked("kamień");
+});
+
+buttonPaper = document.getElementById("button-paper");
+buttonPaper.addEventListener("click", function () {
+    buttonClicked("papier");
+});
+
+buttonScissors = document.getElementById("button-scissors");
+buttonScissors.addEventListener("click", function () {
+    buttonClicked("nożyce");
+});
 /**
  * Describe this function...
  */
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(argButtonName + " został kliknięty");
+
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
   if (argMoveId == 1) {
@@ -35,9 +53,8 @@ function displayResult(argPlayerMove, argComputerMove) {
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+playerMove = argButtonName;
 console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
 console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
